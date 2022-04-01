b5edebcec389f6167311b57102bddbe2d943c6e3
Bruno Sajermann <sajermannbruno@gmail.com>
2022-04-01 08:32:26 -0300

    Module 9 Part 2 Video 3 Complete

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,56 @@
-import Title from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+
+import logoImg from '../../assets/github-logo.svg';
 
 export default function Dashboard() {
-	return <Title>Explore Repositórios no Github</Title>;
+	return (
+		<>
+			<img src={logoImg} alt="Github Explorer" />
+			<Title>Explore Repositórios no Github</Title>
+			<Form>
+				<input placeholder="Digite o nome do repositório" />
+				<button type="submit">Pesquisar</button>
+			</Form>
+
+			<Repositories>
+				<a href="teste">
+					<img
+						src="https://avatars.githubusercontent.com/u/50279825?v=4"
+						alt="Sajermann"
+					/>
+					<div>
+						<strong>Bruno Sajermann</strong>
+						<p>Descrição do Repo</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</a>
+				<a href="teste">
+					<img
+						src="https://avatars.githubusercontent.com/u/50279825?v=4"
+						alt="Sajermann"
+					/>
+					<div>
+						<strong>Bruno Sajermann</strong>
+						<p>Descrição do Repo</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</a>
+				<a href="teste">
+					<img
+						src="https://avatars.githubusercontent.com/u/50279825?v=4"
+						alt="Sajermann"
+					/>
+					<div>
+						<strong>Bruno Sajermann</strong>
+						<p>Descrição do Repo</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</a>
+			</Repositories>
+		</>
+	);
 }
