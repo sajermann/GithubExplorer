de8e0835e39048eefe5349d5b8aabe08413fe570
Bruno Sajermann <sajermannbruno@gmail.com>
2022-04-04 08:18:25 -0300

    Module 9 Part 2 Video 6 Complete

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import { Title, Form, Repositories, Error } from './styles';
 import api from '../../services/api';
@@ -17,7 +17,20 @@ interface Repository {
 export default function Dashboard() {
 	const [newRepo, setNewRepo] = useState('');
 	const [inputError, setInputError] = useState('');
-	const [repositories, setRepositories] = useState<Repository[]>([]);
+	const [repositories, setRepositories] = useState<Repository[]>(() => {
+		const storaged = localStorage.getItem('@GithubExplorer:repositories');
+		if (storaged) {
+			return JSON.parse(storaged);
+		}
+		return [];
+	});
+
+	useEffect(() => {
+		localStorage.setItem(
+			'@GithubExplorer:repositories',
+			JSON.stringify(repositories)
+		);
+	}, [repositories]);
 
 	async function handleAddRepository(e: FormEvent<HTMLFormElement>) {
 		e.preventDefault();
